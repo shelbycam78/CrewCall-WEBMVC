6137301629be444767400d8b57f4533661ae8034
Shelby Simpson <shelbycam78@gmail.com>
2021-11-01 19:21:06 -0400

    Add project files.

---
+++ BAPapp.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BAPapp.WebMVC.Startup))]
+namespace BAPapp.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
